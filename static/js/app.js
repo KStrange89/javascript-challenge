57d639a31533be3e2070fb46c9b1e91991641515
Katie Strange <katie.lynn.strange.89@gmail.com>
2021-06-14 07:28:35 -0600

    Got it working

--- static/js/app.js
+++ static/js/app.js
@@ -3,25 +3,23 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-console.log(data);
-
-// Step 5: Use d3 to update each cell's text with
-// weather report values (weekday, date, high, low)
-data.forEach(function(ufoReport) {
-  console.log(ufoReport);
+function CreateTable (data) {
+  tbody.html("");
+  data.forEach(function(ufoReport) {
   var row = tbody.append("tr");
   Object.entries(ufoReport).forEach(function([key, value]) {
-    console.log(key, value);
     var cell = row.append("td");
     cell.text(value);
   });
 });
+}
 
-// // BONUS: Refactor to use Arrow Functions!
-// data.forEach((weatherReport) => {
-//   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(([key, value]) => {
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+CreateTable(tableData);
+
+function filterbythis() {
+  var input = document.getElementById("datetime").value;
+  console.log(input);
+  var filterData = tableData.filter(row => row.datetime === input);
+  console.log(filterData);
+ CreateTable(filterData);
+};
